309f9a5c47d4aac56ebe90a666b52a3ae64db42b
Lucas Ramalho <lucasramalho.dev@gmail.com>
2023-01-11 23:14:16 -0300

    Requests FakeDB

--- Programs/Fake_DB.js
+++ Programs/Fake_DB.js
@@ -1,26 +1,51 @@
-
 //"Banco de Dados"
 let DataBase = {
-    cursos: [],
-    ies: []
+    cursos: ["Ciência da Computação", "Odontologia", "ADS"]
 }
 
-//Data
-let curso = "Ciência da Computação"
-let instituição = "UFCG"
+//Faça sua requisição:
+// update = nome do curso - string
+// delete = index do curso - number
+// get = index do curso - number
+let request_type = "get"
+let curso = 1;
 
-//controler
-function Update(data, db) {
-    let update = db.push(data)
-    return update
+
+//Controll
+if(request_type == "update"){
+    function Update(data, db) {
+        let update = db.push(data)
+        return update;
+    }
+
+    Update(curso, DataBase.cursos)
+
+    let cursos = DataBase.cursos;
+
+    console.log(`Você adicionou ${curso} na tabela cursos\nTabela Atualizada: ${cursos}`)
+
+}else if(request_type == "delete"){
+    function Delete(id, db) {
+        let del = db.splice(id, id)
+        return del;
+    }
+
+    Delete(curso, DataBase.cursos)
+
+    let cursos = DataBase.cursos;
+
+    console.log(`Você deletou a coluna de index: ${curso}, na tabela cursos\nTabela Atualizada: ${cursos}`)
+
+}else if(request_type == "get") {
+    function Get(id, db){
+        let get = db.shift(id)
+        return get
+    }
+    let get_curso = Get(curso ,DataBase.cursos)
+
+    let cursos = DataBase.cursos;
+    
+    console.log(`Você consultou a coluna de index: ${curso}, na tabela cursos\nConsulta: ${get_curso}\n`)
 }
 
-Update(curso, DataBase.cursos)
-Update(instituição, DataBase.ies)
-
-let cursos = DataBase.cursos
-let ies = DataBase.ies
-
-console.log(cursos)
-console.log(ies)
 
