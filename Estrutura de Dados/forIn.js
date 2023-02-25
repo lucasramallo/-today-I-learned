c2b99995521efdf083e56d44544d9511cf52cc47
lucasramallo <lucasdebrito.jd@gmail.com>
2023-02-24 23:08:31 -0300

    For In

--- Estrutura de Dados/forIn.js
+++ Estrutura de Dados/forIn.js
@@ -10,27 +10,33 @@ const team = [
     {
         name: "Lucas", 
         function: "Front-end Developer",
+        stacks: ["Type Script", "React", "Next", "Tailwind", "Styled Components"]
     },
     {
         name: "Rodrigo", 
-        function: "Front-end Developer"
+        function: "Front-end Developer",
+        stacks: ["Type Script", "React", "Next", "Tailwind", "Styled Components"]
     },
     {
         name: "Filipe", 
-        function: "Back-end Developer"
+        function: "Back-end Developer",
+        stacks: ["Type Script", "Node", "Nest", "Elixir", "Fastfy", "Postgress", "Prisma"]
     },
     {
         name: "Amanda", 
-        function: "Back-end Developer"
+        function: "Back-end Developer",
+        stacks: ["Type Script", "Node", "Nest", "Postgress", "MySql", "Graphql", "Prisma"]
     },
     {
         name: "Aline", 
-        function: "UI/UX"
+        function: "UI/UX",
+        stacks: ["Adobe XD", "Figma", "Blender", "Cinema 4D"]
     }
 ]
 
 for(member in team) {
     console.log(`Name: ${team[member].name}`)
-    console.log(`Function: ${team[member].function}\n`)
+    console.log(`Function: ${team[member].function}`)
+    console.log(`Stack: ${team[member].stacks} \n`)
 }
 
