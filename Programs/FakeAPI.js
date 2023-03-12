caea18a8c1faf5a7b610146a5405105287051b68
lucasramallo <lucasdebrito.jd@gmail.com>
2023-03-11 22:03:53 -0300

    Fake API

--- Programs/FakeAPI.js
+++ Programs/FakeAPI.js
@@ -1,22 +1,22 @@
 const DataBase = {
-    Members: [
+    Users: [
         {
             name: "Jonh Doe", 
-            age: 24, 
+            nota: 80, 
             cpf: "132.112.321.32", 
             level: "junior", 
             salario: "3.500"
         },
         {
             name: "Diego Martins", 
-            age: 37, 
+            nota: 97, 
             cpf: "273.998.142.01", 
             level: "senior", 
             salario: "12.850"
         },
         {
             name: "Filipe Fernandes", 
-            age: 28, 
+            nota: 65, 
             cpf: "745.208.624.75", 
             level: "pleno", 
             salario: "7.200"
@@ -24,13 +24,18 @@ const DataBase = {
     ]
 } 
 
-const teamMembers = DataBase.Members
+const Users = DataBase.Users
 
-const Member = (teamMembers) => {
-    
-   for(member of teamMembers){
-        console.log(`Nome: ${member.name}\nIdade: ${member.age}\nCpf: ${member.cpf}\nNivel: ${member.level}\nSalário: R$${member.salario}\n`)
+console.log('Total de Usuarios')
+const totalUsers = Users.forEach((_, index) => {
+    console.log(index +1)
+})
+
+const approvedUsers = Users.forEach((value) => {
+
+    if(value.nota > 70){
+        console.log(`\n${value.name}`)
+        console.log(`Nota${value.nota} \n`)
     }
+})
 
-}
-Member(teamMembers)
