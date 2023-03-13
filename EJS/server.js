6782b6e153faae1bdf86c13b96e1afa30a628e5b
lucasramallo <lucasdebrito.jd@gmail.com>
2023-03-13 11:54:23 -0300

    SQL

--- EJS/server.js
+++ EJS/server.js
@@ -1,17 +1,17 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("index");
 });
 
 app.get("/sobre", (req, res) => {
-    res.render("about")
+    res.render("about");
 });
 
-const port = 8080
+const port = 8080;                                                                                                                     
 app.listen(port);
 
-console.log(`✅Running! In port ${port}🔥`)
+console.log(`✅Running! In port ${port}🔥`);
