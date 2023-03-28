cff0b11a959add031359d6be7b3d2bbec6ab6d80
lucasramallo <lucasdebrito.jd@gmail.com>
2023-03-28 12:08:41 -0300

    Pair or Odd in Python

---
+++ ParOrImpar.py
@@ -0,0 +1,10 @@
+number = input('Informe um número, por favor! |> ')
+
+stringToNumber = int(number)
+
+restOfDivision = stringToNumber%2
+
+if restOfDivision == 0:
+    print('This number is PAIR.')
+else: 
+    print('This number is ODD.')
