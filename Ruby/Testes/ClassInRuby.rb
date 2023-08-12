bcaa19439397846680a6b8df6c3208b0169cbad5
lucasramallo <lucasdebrito.jd@gmail.com>
2023-08-12 09:55:07 -0300

    classe

--- Ruby/Testes/ClassInRuby.rb
+++ Ruby/Testes/ClassInRuby.rb
@@ -1,7 +1,19 @@
 class User 
-    def initializer(name, email, age) 
+    def initialize(name, email, age) 
         @name = name
         @email = email
         @age = age
     end
+
+    def get()
+        puts @name
+        puts @email
+        puts @age
+    end
+
 end
+
+
+user = User.new("Lucas", "lucasdebrito2.jd@gmail.com", 18)
+
+user.get()
